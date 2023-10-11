8eb34cb5c99b63cba3addfc5469399c32d451043
Azizbek Jorayev <jorayevazizbek424@gmail.com>
2023-10-11 08:47:03 +0500

    Add files via upload

---
+++ Furniture/includes/header.php
@@ -0,0 +1,36 @@
+<div class="header_section">
+         <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+               <a class="logo" href="index.php"><img src="images/logo.png"></a>
+               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+               </button>
+               <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                     <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="about.php">About</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="design.php">Design</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="shop.php">Shop</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact</a>
+                     </li>
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0">
+                     <div class="search_icon">
+                        <ul>
+                           <li><a href="#"><img src="images/search-icon.png"></a></li>
+                           <li><a href="#"><img src="images/user-icon.png"></a></li>
+                        </ul>
+                     </div>
+                  </form>
+               </div>
+            </nav>
+         </div>
